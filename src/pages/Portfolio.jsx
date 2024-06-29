3a7bb5aa9eb994f1a4c09ae8c5c155da4b3a6ff8
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-06-29 14:40:46 +0530

    integrted fyers apis

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,67 +1,19 @@
-// import React, { useState, useEffect } from "react";
-// import AccountInfo from "../components/brokers/fyers/AccountInfo";
-// import Header from "../components/brokers/fyers/Header";
-// import BrokerModal from "../components/brokers/fyers/BrokerModal";
-// import StockDetails from "../components/brokers/fyers/portfolio/StockDetails";
-
-// function Portfolio() {
-//   // const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   // useEffect(() => {
-//   //   setIsModalOpen(true);
-//   // }, []);
-
-//   // const closeModal = () => {
-//   //   setIsModalOpen(false);
-//   // };
-
-//   return (
-//     <div className="-z-10">
-//       <div className="min-h-screen lg:px-32 p-4 relative">
-//         <img
-//           loading="lazy"
-//           className="absolute -z-10 top-1/2 transform -translate-y-1/2 left-[0] w-[165px]"
-//           src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2F87dfd2fd4eea4f378d9e578d4c5dd7d0"
-//           alt="bull"
-//         />
-//         <img
-//           loading="lazy"
-//           className="absolute -z-10 top-1/2 transform -translate-y-1/2 right-[0px] w-[160px]"
-//           src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2F9815d9f59dfd4f65b9e50d5dcbb0152c"
-//           alt="bear"
-//         />
-
-//         <div className="bg-white min-h-[85vh] news-table rounded-2xl">
-//           <div className="py-5 px-5 flex flex-col rounded-2xl">
-//             <Header />
-//             <AccountInfo />
-//             <StockDetails />
-//           </div>
-//         </div>
-//       </div>
-//       {/* <BrokerModal isOpen={isModalOpen} onClose={closeModal} /> */}
-//     </div>
-//   );
-// }
-
-// export default Portfolio;
-
 import React, { useState, useEffect } from "react";
 import AccountInfo from "../components/brokers/fyers/AccountInfo";
 import Header from "../components/brokers/fyers/Header";
+import BrokerModal from "../components/brokers/fyers/BrokerModal";
 import StockDetails from "../components/brokers/fyers/portfolio/StockDetails";
 
 function Portfolio() {
-  const [accessToken, setAccessToken] = useState("");
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (!token) {
-      console.log("No access token found. Display message to connect broker.");
-    } else {
-      setAccessToken(token); // Set access token state if present
-    }
-  }, []);
+  // useEffect(() => {
+  //   setIsModalOpen(true);
+  // }, []);
+
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
   return (
     <div className="-z-10">
@@ -82,19 +34,12 @@ function Portfolio() {
         <div className="bg-white min-h-[85vh] news-table rounded-2xl">
           <div className="py-5 px-5 flex flex-col rounded-2xl">
             <Header />
-            {accessToken ? (
-              <>
-                <AccountInfo />
-                <StockDetails />
-              </>
-            ) : (
-              <p className="text-center p-3">
-                Select your broker to view your portfolio.
-              </p>
-            )}
+            <AccountInfo />
+            <StockDetails />
           </div>
         </div>
       </div>
+      {/* <BrokerModal isOpen={isModalOpen} onClose={closeModal} /> */}
     </div>
   );
 }
