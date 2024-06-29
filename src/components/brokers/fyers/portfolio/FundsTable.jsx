4ee8968f5768a68f94d9efd23ba8cb09fc84ef96
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-06-29 18:06:45 +0530

    updated funds and trade table

--- src/components/brokers/fyers/portfolio/FundsTable.jsx
+++ src/components/brokers/fyers/portfolio/FundsTable.jsx
@@ -1,6 +1,97 @@
+// import React, { useEffect, useState } from "react";
+// import { fetchFunds } from "../api";
+// import Loading from "../../../common/Loading";
+
+// const FundsTable = () => {
+//   const [funds, setFunds] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const getFundsData = async () => {
+//       try {
+//         const response = await fetchFunds();
+//         if (response.s === "ok") {
+//           setFunds(response.fund_limit);
+//           setLoading(false); 
+//         } else {
+//           setError(response.message); 
+//           setLoading(false); 
+//         }
+//       } catch (error) {
+//         console.error("Error fetching funds:", error);
+//         setError("Failed to fetch funds. Please authenticate and try again....");
+//         setLoading(false); 
+//       }
+//     };
+
+//     getFundsData();
+//   }, []);
+
+//   if (loading) {
+//     return <div className="text-center p-4"><Loading/></div>;
+//   }
+
+//   if (error) {
+//     return <div className="text-center p-4 text-red-500">{error}</div>;
+//   }
+
+//   if (!funds || funds.length === 0) {
+//     return <div className="text-center p-4">There are no funds</div>;
+//   }
+
+//   // Columns to exclude
+//   const excludedColumns = [];
+
+//   let columnNames = Object.keys(funds[0]).filter(
+//     (columnName) => !excludedColumns.includes(columnName)
+//   );
+
+//   // Ensure 'title' is the first column
+//   columnNames = ["title", ...columnNames.filter((col) => col !== "title")];
+
+//   return (
+//     <div className="h-[55vh] overflow-auto">
+//       <table className="min-w-full border-collapse">
+//         <thead>
+//           <tr>
+//             {columnNames.map((columnName) => (
+//               <th
+//                 key={columnName}
+//                 className="px-4 capitalize whitespace-nowrap overflow-hidden py-2 font-[poppins] text-sm font-normal text-[#FFFFFF99] text-left"
+//               >
+//                 {columnName}
+//               </th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {funds.map((fund, index) => (
+//             <tr key={index}>
+//               {columnNames.map((columnName) => (
+//                 <td
+//                   key={`${columnName}-${index}`}
+//                   className={`px-4 whitespace-nowrap overflow-hidden font-semibold py-4 ${
+//                     columnName === "title" ? "text-[#6FD4FF]" : ""
+//                   }`}
+//                 >
+//                   {fund[columnName]}
+//                 </td>
+//               ))}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default FundsTable;
+
+
 import React, { useEffect, useState } from "react";
-import { fetchFunds } from "../api";
 import Loading from "../../../common/Loading";
+import api from "../../../../config.js";
 
 const FundsTable = () => {
   const [funds, setFunds] = useState([]);
@@ -10,18 +101,17 @@ const FundsTable = () => {
   useEffect(() => {
     const getFundsData = async () => {
       try {
-        const response = await fetchFunds();
-        if (response.s === "ok") {
-          setFunds(response.fund_limit);
-          setLoading(false); 
+        const response = await api.get("/api/v1/fyers/fetchFunds");
+        if (response.data.s === "ok") {
+          setFunds(response.data.fund_limit);
         } else {
-          setError(response.message); 
-          setLoading(false); 
+          setError(response.data.message);
         }
       } catch (error) {
         console.error("Error fetching funds:", error);
         setError("Failed to fetch funds. Please authenticate and try again....");
-        setLoading(false); 
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -40,14 +130,12 @@ const FundsTable = () => {
     return <div className="text-center p-4">There are no funds</div>;
   }
 
-  // Columns to exclude
   const excludedColumns = [];
 
   let columnNames = Object.keys(funds[0]).filter(
     (columnName) => !excludedColumns.includes(columnName)
   );
 
-  // Ensure 'title' is the first column
   columnNames = ["title", ...columnNames.filter((col) => col !== "title")];
 
   return (
--- src/components/brokers/fyers/portfolio/TradesTable.jsx
+++ src/components/brokers/fyers/portfolio/TradesTable.jsx
@@ -1,6 +1,87 @@
+// import React, { useEffect, useState } from "react";
+// import { fetchTrades } from "../api";
+// import Loading from "../../../common/Loading";
+
+// const TradesTable = () => {
+//   const [trades, setTrades] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchTradesData = async () => {
+//       try {
+//         const response = await fetchTrades();
+//         if (response.s === "ok") {
+//           setTrades(response.tradeBook);
+//           setLoading(false); // Set loading to false once data is fetched
+//         } else {
+//           setError(response.message); // Set error message if API request fails
+//           setLoading(false); // Set loading to false in case of error
+//         }
+//       } catch (error) {
+//         console.error("Error fetching trades:", error);
+//         setError("Failed to fetch trades. Please authenticate and try again....."); // Set error message for network or other errors
+//         setLoading(false); // Set loading to false in case of error
+//       }
+//     };
+
+//     fetchTradesData();
+//   }, []);
+
+//   if (loading) {
+//     return <div className="text-center p-4"><Loading/></div>;
+//   }
+
+//   if (error) {
+//     return <div className="text-center p-4 text-red-500">{error}</div>;
+//   }
+
+//   if (!trades || trades.length === 0) {
+//     return <div className="text-center p-4">There are no trades</div>;
+//   }
+
+//   // Extract column names dynamically from the first trade object
+//   const columnNames = Object.keys(trades[0]);
+
+//   return (
+//     <div className="h-[55vh] overflow-auto">
+//       <table className="min-w-full border-collapse">
+//         <thead>
+//           <tr>
+//             {columnNames.map((columnName) => (
+//               <th
+//                 key={columnName}
+//                 className="px-4 whitespace-nowrap capitalize py-3 font-[poppins] text-sm font-normal text-[#FFFFFF99] text-left"
+//               >
+//                 {columnName}
+//               </th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {trades.map((trade, index) => (
+//             <tr key={index} className="text-center">
+//               {columnNames.map((columnName) => (
+//                 <td
+//                   key={`${columnName}-${index}`}
+//                   className="px-4 whitespace-nowrap text-left font-semibold py-4"
+//                 >
+//                   {trade[columnName]}
+//                 </td>
+//               ))}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default TradesTable;
+
 import React, { useEffect, useState } from "react";
-import { fetchTrades } from "../api";
 import Loading from "../../../common/Loading";
+import api from "../../../../config.js";
 
 const TradesTable = () => {
   const [trades, setTrades] = useState([]);
@@ -8,24 +89,23 @@ const TradesTable = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchTradesData = async () => {
+    const getTradesData = async () => {
       try {
-        const response = await fetchTrades();
-        if (response.s === "ok") {
-          setTrades(response.tradeBook);
-          setLoading(false); // Set loading to false once data is fetched
+        const response = await api.get("/api/v1/fyers/fetchTrades");
+        if (response.data.s === "ok") {
+          setTrades(response.data.tradeBook);
         } else {
-          setError(response.message); // Set error message if API request fails
-          setLoading(false); // Set loading to false in case of error
+          setError(response.data.message);
         }
       } catch (error) {
         console.error("Error fetching trades:", error);
-        setError("Failed to fetch trades. Please authenticate and try again....."); // Set error message for network or other errors
-        setLoading(false); // Set loading to false in case of error
+        setError("Failed to fetch trades. Please authenticate and try again.");
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchTradesData();
+    getTradesData();
   }, []);
 
   if (loading) {
@@ -40,7 +120,6 @@ const TradesTable = () => {
     return <div className="text-center p-4">There are no trades</div>;
   }
 
-  // Extract column names dynamically from the first trade object
   const columnNames = Object.keys(trades[0]);
 
   return (
@@ -78,3 +157,4 @@ const TradesTable = () => {
 };
 
 export default TradesTable;
+
