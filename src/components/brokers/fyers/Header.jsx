29958b6374de0f5e28eefb8e6cbe3ea09eb2bcbe
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-06-30 16:13:51 +0530

    testing on landing page

--- src/components/brokers/fyers/Header.jsx
+++ src/components/brokers/fyers/Header.jsx
@@ -1,3 +1,5 @@
+// src/components/header/Header.jsx
+
 import React, { useEffect, useState } from "react";
 import Dropdown from "../../common/Dropdown";
 import { useNavigate } from "react-router-dom";
@@ -49,34 +51,38 @@ function Header() {
   return (
     <div className="flex justify-between items-center pb-2 border-b">
       <h1 className="font-[poppins] font-semibold">Account Manager</h1>
-      <div>
-            <button onClick={handleFyersAuth}>Authenticate with Fyers</button>
-          </div>
-      <div className="max-h-10">
-        {selectedOption === "Fyers" && (
-          <img
-            className="h-16"
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fb0f59404dbfe4720b7475114d61a6db6"
-            alt="Fyers"
-          />
-        )}
-        {selectedOption === "Zerodha" && (
-          <img
-            className="h-8"
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fd314830a17a84ee5956be4ca2cee3c5a"
-            alt="Zerodha"
-          />
-        )}
-        {selectedOption === "Motilal Oswal" && (
-          <img
-            className="h-10"
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fe9a3e857c17e41ceac69d3e2acc20695"
-            alt="Motilal Oswal"
-          />
-        )}
+      <div className="flex items-center">
+        <div className="max-h-10">
+          {selectedOption === "Fyers" && (
+            <div className="flex items-center">
+              <img
+                className="h-16"
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fb0f59404dbfe4720b7475114d61a6db6"
+                alt="Fyers"
+              />
+              <button onClick={handleFyersAuth} className="ml-4">
+                Authenticate with Fyers
+              </button>
+            </div>
+          )}
+          {selectedOption === "Zerodha" && (
+            <img
+              className="h-8"
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fd314830a17a84ee5956be4ca2cee3c5a"
+              alt="Zerodha"
+            />
+          )}
+          {selectedOption === "Motilal Oswal" && (
+            <img
+              className="h-10"
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets%2F462dcf177d254e0682506e32d9145693%2Fe9a3e857c17e41ceac69d3e2acc20695"
+              alt="Motilal Oswal"
+            />
+          )}
+        </div>
       </div>
       <div>
         <Dropdown
