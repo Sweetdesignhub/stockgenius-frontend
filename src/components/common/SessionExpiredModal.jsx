867fed98d0ca5a2520f6d373ea1af3d78d10b1c0
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-09-14 13:32:27 +0530

    resolved z indes for session expire modal

--- src/components/common/SessionExpiredModal.jsx
+++ src/components/common/SessionExpiredModal.jsx
@@ -1,16 +1,21 @@
 // SessionExpiredModal.jsx
-import React from 'react';
-import { Dialog } from '@headlessui/react'; // Assuming you're using @headlessui for modal
+import React from "react";
+import { Dialog } from "@headlessui/react"; // Assuming you're using @headlessui for modal
 
 const SessionExpiredModal = ({ showModal, onSignOut }) => {
   if (!showModal) return null;
 
   return (
     <Dialog open={showModal} onClose={() => {}}>
-      <div className="fixed inset-0 bg-black bg-opacity-30" aria-hidden="true" />
-      <div className="fixed inset-0 flex items-center justify-center p-4">
+      <div
+        className="fixed inset-0 bg-black bg-opacity-30"
+        aria-hidden="true"
+      />
+      <div className="fixed z-30 inset-0 flex items-center justify-center p-4">
         <Dialog.Panel className="max-w-sm mx-auto bg-white px-5 py-5 rounded-xl shadow-lg">
-          <Dialog.Title className="text-lg text-center font-bold text-black">Session Expired</Dialog.Title>
+          <Dialog.Title className="text-lg text-center font-bold text-black">
+            Session Expired
+          </Dialog.Title>
           <Dialog.Description className="mt-2 text-black">
             Your session has expired. Please sign in again.
           </Dialog.Description>
