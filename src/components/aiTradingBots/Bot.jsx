055fa9e5b161fd8b2f6bb7a8aefb976d785057e2
Arshdeep Singh <singharshdeep9039@gmail.com>
2025-05-16 23:50:46 +0530

    Trigger redeploy

--- src/components/aiTradingBots/Bot.jsx
+++ src/components/aiTradingBots/Bot.jsx
@@ -327,7 +327,7 @@ function Bot({
       setModalMessage(newMessage);
       setConfirmAction("toggle");
       setYesNoModalOpen(true);
-    }
+    } 
   };
 
   //  API endpoint based on the bot type
