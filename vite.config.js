0e904c2a2ef5db705219bef4315e800e83761155
Arshdeep Singh <singharshdeep9039@gmail.com>
2025-06-16 12:46:08 +0530

    updated yml file to resolve build error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       "/api": {
