7fa23d86506af277b36c5f08aa205b37f9db43f9
Atharva1704 <atharva2003chavan@gmail.com>
2025-07-03 12:33:26 +0530

    Adding the USA plan

--- src/components/pricing/PricingDialog.jsx
+++ src/components/pricing/PricingDialog.jsx
@@ -8,8 +8,9 @@ import PRICING_PLANS from "./PricingPlansValues";
 export default function PricingDialog({ isOpen = false, onClose }) {
   const { currentUser } = useSelector((state) => state.user);
   const region = useSelector((state) => state.region);
+  const currentPlan =
+    region === "india" ? currentUser?.plan : currentUser?.planUsa;
 
-  const currentPlan = currentUser?.plan || "basic"; // Fallback to "basic" if undefined
   // console.log(currentUser);
   const [showPlanDialog, setShowPlanDialog] = useState(false);
   const [selectedPlan, setSelectedPlan] = useState(
